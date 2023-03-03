717f044b74e2d9b7c39296d8438d2b20a2046fbe
haobosang <lihaobo1998@gmail.com>
2023-03-03 16:04:52 +0800

    Tensor Completed

--- source/data/Tensor.cpp
+++ source/data/Tensor.cpp
@@ -18,9 +18,6 @@ Tensor<float> &Tensor<float>::operator=(const Tensor &tensor){
         this->raw_shapes_ = tensor.raw_shapes_;
     }
     return *this;
-
-
-
 }
 void Tensor<float>::set_data(const arma::fcube &data){
     CHECK(data.n_rows==this->data_.n_rows) << data.n_rows << " != " << this->data_.n_rows;
@@ -55,53 +52,141 @@ uint32_t Tensor<float>::size() const{
 
 }
 float Tensor<float>::index(uint32_t offset) const{
+    CHECK(offset < this->data_.size());
+    return this->data_.at(offset);
 
 }
+//shapes目前有BUG，传引用返回数组更快
 std::vector<uint32_t> Tensor<float>::shapes() const{
+    CHECK(!this->data_.empty());
+    return {this->channels(),this->rows(),this->cols()};
 
 }
 arma::fcube &Tensor<float>::data(){
     return this->data_;
 }
 const arma::fcube &Tensor<float>::data() const{
-
+    return this->data_;
 }
 
+arma::fmat &Tensor<float>::at(uint32_t channel){
+    CHECK_LT(channel,this->channels());
+    return this->data_.slice(channel);
+}
+
+const arma::fmat &Tensor<float>::at(uint32_t channel) const{
+    CHECK_LT(channel,this->channels());
+    return this->data_.slice(channel);
+}
+
+
 float Tensor<float>::at(uint32_t channel, uint32_t row, uint32_t col) const{
+    CHECK_LE(col,this->cols());
+    CHECK_LT(row,this->rows());
+    CHECK_LT(channel,this->channels());
+    return this->data_.at(row,col,channel);
 
 }
 
 float &Tensor<float>::at(uint32_t channel, uint32_t row, uint32_t col){
-
+    CHECK_LE(col,this->cols());
+    CHECK_LT(row,this->rows());
+    CHECK_LT(channel,this->channels());
+    return this->data_.at(row,col,channel);
 }
 
-void Tensor<float>::Padding(const std::vector<uint32_t> &pads, float padding_value){
-
-}
 
 void Tensor<float>::Fill(float value){
-    this->data_.fill();
+    CHECK(!this->data_.empty());
+    this->data_.fill(value);
+
+}
+void Tensor<float>::Padding(const std::vector<uint32_t> &pads, float padding_value){
+    CHECK(!this->data_.empty());
+    CHECK_EQ(pads.size(), 4);
+    uint32_t pad_rows1 = pads.at(0);  // up
+    uint32_t pad_rows2 = pads.at(1);  // bottom
+    uint32_t pad_cols1 = pads.at(2);  // left
+    uint32_t pad_cols2 = pads.at(3);  // right
+
+    this->data_.insert_rows(0,pad_rows1);
+    this->data_.insert_rows(this->data_.n_rows,pad_rows2);
+    this->data_.insert_cols(0,pad_cols1);
+    this->data_.insert_cols(this->data_.n_cols,pad_cols2);
+    this->raw_shapes_ = this->shapes();
+    
 
 }
 
 void Tensor<float>::Fill(const std::vector<float> &values){
+    CHECK(!this->data_.empty());
+    const uint32_t total_elems = this->data_.size();
+    CHECK_EQ(values.size(), total_elems);
 
-}
+    const uint32_t rows = this->rows();
+    const uint32_t cols = this->cols();
+    const uint32_t planes = rows * cols;
+    const uint32_t channels = this->data_.n_slices;
 
-void Tensor<float>::Ones(){
 
-}
-
-void Tensor<float>::Rand(){
-
-}
-
-void Tensor<float>::Show(){
+    for(uint32_t i =0;i<channels;++i)
+    {
+        auto &channel_data = this->data_.slice(i);
+        const arma::fmat &channel_data_t = arma::fmat(values.data()+i*planes,this->cols(),this->rows());
+        channel_data = channel_data_t.t();
+    }
 
 }
 
 void Tensor<float>::Flatten(){
+    CHECK(!this->data_.empty());
+    const uint32_t size = this->data_.size();
+    arma::fcube linear_cube(size,1,1); 
 
+
+    const uint32_t row = this->rows();
+    const uint32_t col = this->cols();
+    const uint32_t channel = this->channels();
+    uint32_t index = 0;
+    for(int c = 0;c<channel;++c)
+    {
+        const arma::fmat &mat = this->data_.slice(c);
+        for(uint32_t r = 0;r<row;++r)
+        {
+            for(uint32_t c = 0;c<col;++c)
+            {
+                linear_cube.at(index,0,0) = mat.at(r,c);
+                index+=1;
+            }
+        }
+    }
+    CHECK_EQ(index,size);
+    this->data_ = linear_cube;
+    this->raw_shapes_ = std::vector<uint32_t>{size};
+
+    
+
+}
+
+void Tensor<float>::Ones(){
+    CHECK(!this->data_.empty());
+    this->data_.fill(1.);
+    return ;
+}
+
+void Tensor<float>::Rand(){
+    CHECK(!this->data_.empty());
+    this->data_.randn();
+    return ;
+
+}
+
+void Tensor<float>::Show(){
+    for(uint32_t i =0;i<this->channels();++i)
+    {
+        std::cout<<"Channel:"<<i;
+        std::cout<<std::endl<<this->data_.slice(i);
+    }
 }
 
 }
--- test/test_tensor.cpp
+++ test/test_tensor.cpp
@@ -3,16 +3,98 @@
 #include<gtest/gtest.h>
 #include<armadillo>
 #include<glog/logging.h>
+#include<vector>
 using namespace TinyTensor;
+// int main(){
+//   Tensor<float> tensor(3, 32, 32);
+//   // ASSERT_EQ(tensor.channels(), 3);
+//   // ASSERT_EQ(tensor.rows(), 32);
+//   // ASSERT_EQ(tensor.cols(), 32);
+//   // Tensor<float> tensor(1,5,5);
+//   // //uint32_t a=128;
+//   // tensor.Rand();
+//   // tensor.Show();
+//   // tensor.Flatten();
+//   // tensor.Show();
+//   // std::vector<uint32_t> b(tensor.shapes());
+//   //arma::fcube data(3,12,12);
+//   //tensor.set_data(data);
+//   // std::cout<<tensor.channels()<<std::endl;
+//   // std::cout<<tensor.cols()<<std::endl;
+//   // std::cout<<tensor.rows()<<std::endl;
+//   // std::cout<<tensor.index(12)<<std::endl;
+//   // std::cout<<b.size()<<std::endl;
+//   // std::cout<<tensor.empty()<<std::endl;
+//   // std::cout<<tensor.size()<<std::endl;
+//   //std::cout<<tensor.index(a)<<std::endl;
+//   //b = tensor.shapes();
+//   // for(auto it:b)  std::cout<<it<<" ";
+//   // for(int i=0;i<3;i++)
+//   // {
+//   //   std::cout<<b[i]<<std::endl;
+//   // }
+ 
+//   //putchar(10);
+//   return 0;mak
+// }
+
+// TEST(test_tensor, create) {
+//   //using namespace kuiper_infer;
+//   Tensor<float> tensor(3, 32, 32);
+//   ASSERT_EQ(tensor.channels(), 3);
+//   ASSERT_EQ(tensor.rows(), 32);
+//   ASSERT_EQ(tensor.cols(), 32);
+// }
+
+// int main(){
+//   Tensor<float> tensor(3, 3, 3);
+//   std::cout<<tensor.channels();
+//   std::cout<<tensor.rows();
+//   std::cout<<tensor.cols();
+
+//   tensor.Fill(1.f); // 填充为1
+//   tensor.Padding({1, 1, 1, 1}, 0); // 边缘填充为0
+//   std::cout<<tensor.rows();
+//   std::cout<<tensor.cols();
+
+//   int index = 0;
+//   // 检查一下边缘被填充的行、列是否都是0
+//   for (int c = 0; c < tensor.channels(); ++c) {
+//     for (int r = 0; r < tensor.rows(); ++r) {
+//       for (int c_ = 0; c_ < tensor.cols(); ++c_) {
+//         if (c_ == 0 || r == 0) {
+//           std::cout<<tensor.at(c, r, c_);
+//         }
+//         index += 1;
+//       }
+//     }
+//   }
+//   std::cout<< "Test2 passed!";
+//   return 0;
+// }
+
 int main(){
-  Tensor<float> tensor(3,12,12);
-  //arma::fcube data(3,12,12);
-  //tensor.set_data(data);
-  std::cout<<tensor.channels()<<std::endl;
-  std::cout<<tensor.cols()<<std::endl;
-  std::cout<<tensor.rows()<<std::endl;
-  
-  std::cout<<tensor.empty()<<std::endl;
-  std::cout<<tensor.size()<<std::endl;
-  return 0;
+
+  Tensor<float> tensor(3, 3, 3);
+  std::cout<< tensor.channels();
+  std::cout<< tensor.rows();
+  std::cout<< tensor.cols();
+
+  std::vector<float> values;
+  for (int i = 0; i < 27; ++i) {
+    values.push_back((float) i);
+  }
+  tensor.Fill(values);
+  std::cout<<  tensor.data();
+
+  int index = 0;
+  for (int c = 0; c < tensor.channels(); ++c) {
+    for (int r = 0; r < tensor.rows(); ++r) {
+      for (int c_ = 0; c_ < tensor.cols(); ++c_) {
+        std::cout<< tensor.at(c, r, c_);
+        index += 1;
+      }
+    }
+  }
+  std::cout<< "Test1 passed!";
 }
