fad8a24299dbbbbd37ffd03816d50f5f7b044f0c
haobosang <lihaobo1998@gmail.com>
2023-03-08 18:00:42 +0800

    LayerRegisterer

--- include/factory/layer_factory.hpp
+++ include/factory/layer_factory.hpp
@@ -4,19 +4,27 @@
 #include "ops/op.hpp"
 #include "layer/layer.hpp"
 
-namespace TinyTensor
-{
-    
-// class LayerRegisterer 
-// {
-// private:
-//     /* data */
-// public:
-//     explicit LayerRegisterer(/* args */);
-//     ~LayerRegisterer();
-// };
+namespace TinyTensor{
 
+class LayerRegisterer {
+public:
+    typedef std::shared_ptr<Layer> (*Creator)(const std::shared_ptr<Operator> &op);
 
+    typedef std::map<OpType,Creator> CreateRegistry;
+
+    static void RegisterCreator(OpType op_type,const Creator &creater);
+
+    static std::shared_ptr<Layer> CreateLayer(const std::shared_ptr<Operator> &op);
+
+    static CreateRegistry &Registry();
+};
+
+class LayerRegistererWrapper {
+ public:
+  LayerRegistererWrapper(OpType op_type, const LayerRegisterer::Creator &creator) {
+    LayerRegisterer::RegisterCreator(op_type, creator);
+  }
+};
 
 } // namespace TinyTensor
 
