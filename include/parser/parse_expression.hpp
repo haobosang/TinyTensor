299f1228cf9776ce223a68a2888c7335c3bcccf9
haobosang <lihaobo1998@gmail.com>
2023-03-13 15:24:47 +0800

    parser head

--- include/parser/parse_expression.hpp
+++ include/parser/parse_expression.hpp
@@ -2,7 +2,7 @@
  * @Author: lihaobo
  * @Date: 2023-03-13 14:14:16
  * @LastEditors: lihaobo
- * @LastEditTime: 2023-03-13 15:10:46
+ * @LastEditTime: 2023-03-13 15:24:18
  * @Description: 请填写简介
  */
 #ifndef TINYTENSOR_INFER_INCLUDE_PARSER_PARSE_EXPRESSION_HPP_
@@ -31,7 +31,14 @@ struct Token{
     int32_t end_pos = 0;
     Token(TokenType token_type, int32_t start_pos, int32_t end_pos):token_type(token_type),start_pos(start_pos),end_pos(end_pos){}
 };
+struct TokenNode{
+    int32_t num_idx = -1;
+    std::shared_ptr<TokenNode> left = nullptr;
+    std::shared_ptr<TokenNode> right = nullptr;
+    TokenNode(int32_t num_idx,std::shared_ptr<TokenNode> left,std::shared_ptr<TokenNode> right);
+    ~TokenNode() = default;
 
+};
 
 
 } // namespace TinyTensor
---
+++ source/parser/parse_expression.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: lihaobo
+ * @Date: 2023-03-13 14:15:01
+ * @LastEditors: lihaobo
+ * @LastEditTime: 2023-03-13 15:23:18
+ * @Description: 请填写简介
+ */
+#include "parser/parse_expression.hpp"
+namespace TinyTensor
+{
+    
+
+    
+} // namespace TinyTensor
