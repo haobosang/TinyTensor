1cd1996eb7ab5f3d5304aaa602050126f79508d4
haobosang <lihaobo1998@gmail.com>
2023-03-22 19:50:51 +0800

    Convolution operator file update

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -2,18 +2,18 @@
  * @Author: lihaobo
  * @Date: 2023-03-21 19:52:25
  * @LastEditors: lihaobo
- * @LastEditTime: 2023-03-21 19:52:43
+ * @LastEditTime: 2023-03-22 19:47:14
  * @Description: 请填写简介
  */
 #include <gtest/gtest.h>
 #include <glog/logging.h>
 
 int main(int argc, char *argv[]) {
-  testing::InitGoogleTest(&argc, argv);
-  google::InitGoogleLogging("TinyTensor");
-  FLAGS_log_dir = "./log/";
-  FLAGS_alsologtostderr = true;
+    testing::InitGoogleTest(&argc, argv);
+    google::InitGoogleLogging("TinyTensor");
+    FLAGS_log_dir = "./log/";
+    FLAGS_alsologtostderr = true;
 
-  LOG(INFO) << "Start test...\n";
-  return RUN_ALL_TESTS();
+    LOG(INFO) << "Start test...\n";
+    return RUN_ALL_TESTS();
 }
