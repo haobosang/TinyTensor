4559d2a81b43f7c0c98ea994cc4b2dbe31c311fc
haobosang <lihaobo1998@gmail.com>
2023-03-22 20:21:27 +0800

    Conv update

---
+++ include/ops/conv_op.hpp
@@ -0,0 +1,37 @@
+/*
+ * @Author: lihaobo
+ * @Date: 2023-03-22 19:47:35
+ * @LastEditors: lihaobo
+ * @LastEditTime: 2023-03-22 20:21:08
+ * @Description: 请填写简介
+ */
+#ifndef TINYTENSOR_OPS_CONV_OP_HPP_
+#define TINYTENSOR_OPS_CONV_OP_HPP_
+#include "op.hpp"
+#include "data/Tensor.hpp"
+#include <vector>
+namespace TinyTensor{
+
+class ConvolutionOp : public Operator{
+
+private:
+    bool bais;
+    uint32_t groups_ = 1;
+    uint32_t padding_h_ = 0;
+    uint32_t padding_w_ = 0;
+    uint32_t stride_h_ = 0;
+    uint32_t stride_w_ = 0;
+    std::vector<Tensor<float>> weight_;
+    std::vector<Tensor<float>> bias_;
+public:
+
+
+
+
+
+};
+
+}
+
+
+#endif //TINYTENSOR_OPS_CONV_OP_HPP_
