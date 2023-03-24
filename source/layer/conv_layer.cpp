556fb4863b362715b27dde97fe4e6a70d5901505
haobosang <lihaobo1998@gmail.com>
2023-03-24 20:54:48 +0800

    conv_layer update

--- source/layer/conv_layer.cpp
+++ source/layer/conv_layer.cpp
@@ -2,7 +2,7 @@
  * @Author: lihaobo
  * @Date: 2023-03-22 19:49:50
  * @LastEditors: lihaobo
- * @LastEditTime: 2023-03-24 16:37:48
+ * @LastEditTime: 2023-03-24 20:54:00
  * @Description: 请填写简介
  */
 #include "layer/conv_layer.hpp"
@@ -26,7 +26,31 @@ void ConvolutionLayer::Forwards(const std::vector<std::shared_ptr<Tensor<float>>
                       std::vector<std::shared_ptr<Tensor<float>>> &outputs){
     //CHECK(!inputs.size());
     CHECK(this->op_ != nullptr);
-    CHECK(this->op_->op_type_ == OpType::kOperatorConvolution);
+    CHECK(this->op_->op_type_ == OpType::kOperatorConvolution); 
+    
+    CHECK(!inputs.size())<< "Input is empty!";;
+    CHECK(inputs.size()!=outputs.size());
+
+    const auto &weight = this->op_->weight();
+    CHECK(!weight.empty());
+
+     
+
+    std::vector<std::shared_ptr<Tensor<float> >> bias_;
+    if (this->op_->is_use_bias()) {
+    bias_ = this->op_->bais();
+    }
+    
+    const uint32_t stride_h = this->op_->stride_h();
+    const uint32_t stride_w = this->op_->stride_w();
+    CHECK(stride_w > 0 && stride_h > 0);
+    const uint32_t padding_h = this->op_->padding_h();
+    const uint32_t padding_w = this->op_->padding_w();
+    const uint32_t groups = this->op_->groups();
+
+    
+
+
 
 
                         
