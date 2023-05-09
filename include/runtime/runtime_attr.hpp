465da5c155edf22257b4124b20f274b05cacacd6
haobosang <lihaobo1998@gmail.com>
2023-05-09 21:48:25 +0800

    runtime and format Update

--- include/runtime/runtime_attr.hpp
+++ include/runtime/runtime_attr.hpp
@@ -5,23 +5,20 @@
  * @LastEditTime: 2023-03-11 14:35:45
  * @Description: 请填写简介
  */
-//
-// Created by fss on 22-11-28.
-//
 
 #ifndef TINYTENSOR_INFER_INCLUDE_PARSER_RUNTIME_ATTR_HPP_
 #define TINYTENSOR_INFER_INCLUDE_PARSER_RUNTIME_ATTR_HPP_
-#include <vector>
-#include <glog/logging.h>
-#include "status_code.hpp"
 #include "runtime_datatype.hpp"
+#include "status_code.hpp"
+#include <glog/logging.h>
+#include <vector>
 
 namespace TinyTensor {
 
 /// 计算图节点的属性信息
 struct RuntimeAttribute {
   std::vector<char> weight_data; /// 节点中的权重参数
-  std::vector<int> shape;  /// 节点中的形状信息
+  std::vector<int> shape;        /// 节点中的形状信息
   RuntimeDataType type = RuntimeDataType::kTypeUnknown; /// 节点中的数据类型
 
   /**
@@ -29,33 +26,32 @@ struct RuntimeAttribute {
    * @tparam T 权重类型
    * @return 权重参数数组
    */
-  template<class T> //
+  template <class T> //
   std::vector<T> get();
 };
 
-template<class T>
-std::vector<T> RuntimeAttribute::get() {
+template <class T> std::vector<T> RuntimeAttribute::get() {
   /// 检查节点属性中的权重类型
   CHECK(!weight_data.empty());
   CHECK(type != RuntimeDataType::kTypeUnknown);
   std::vector<T> weights;
   switch (type) {
-    case RuntimeDataType::kTypeFloat32: { /// 加载的数据类型是float
-      const bool is_float = std::is_same<T, float>::value;
-      CHECK_EQ(is_float, true);
-      const uint32_t float_size = sizeof(float);
-      CHECK_EQ(weight_data.size() % float_size, 0);
-      for (uint32_t i = 0; i < weight_data.size() / float_size; ++i) {
-        float weight = *((float *) weight_data.data() + i);
-        weights.push_back(weight);
-      }
-      break;
+  case RuntimeDataType::kTypeFloat32: { /// 加载的数据类型是float
+    const bool is_float = std::is_same<T, float>::value;
+    CHECK_EQ(is_float, true);
+    const uint32_t float_size = sizeof(float);
+    CHECK_EQ(weight_data.size() % float_size, 0);
+    for (uint32_t i = 0; i < weight_data.size() / float_size; ++i) {
+      float weight = *((float *)weight_data.data() + i);
+      weights.push_back(weight);
     }
-    default: {
-      LOG(FATAL) << "Unknown weight data type";
-    }
+    break;
+  }
+  default: {
+    LOG(FATAL) << "Unknown weight data type";
+  }
   }
   return weights;
 }
-}
-#endif //TINYTENSOR_INFER_INCLUDE_PARSER_RUNTIME_ATTR_HPP_
+} // namespace TinyTensor
+#endif // TINYTENSOR_INFER_INCLUDE_PARSER_RUNTIME_ATTR_HPP_
