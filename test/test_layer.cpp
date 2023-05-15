949a26fb35520f77d7712f27366f01d3a779854f
Ubuntu <ubuntu@ip-172-31-26-164.ap-northeast-1.compute.internal>
2023-05-15 05:24:16 +0000

    sigmoid test update

--- test/test_layer.cpp
+++ test/test_layer.cpp
@@ -1,9 +1,9 @@
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 #include "../source/layer/details/relu.hpp"
+#include "../source/layer/details/sigmoid.hpp"
 #include "runtime/runtime_ir.hpp"
 #include "layer/abstract/layer.hpp"
-#include <benchmark/benchmark.h>
 TEST(test_layer, forward_relu) {
     using namespace TinyTensor;
     std::shared_ptr<Tensor<float>> input = std::make_shared<Tensor<float>>(1, 1, 3);
@@ -24,5 +24,26 @@ TEST(test_layer, forward_relu) {
     ASSERT_EQ(outputs.at(i)->index(1), 0.f);
     ASSERT_EQ(outputs.at(i)->index(2), 3.f);
     }
+}
+TEST(test_layer, forward_sigmoid) {
+    using namespace TinyTensor;
+    std::shared_ptr<Tensor<float>> input = std::make_shared<Tensor<float>>(1, 1, 3);
+    input->index(0) = -1.f;
+    input->index(1) = -2.f;
+    input->index(2) = 3.f;
+    std::vector<std::shared_ptr<Tensor<float>>> inputs;
+    std::vector<std::shared_ptr<Tensor<float>>> outputs;
+    inputs.push_back(input);
+    outputs.push_back(input);
+    SigmoidLayer sigmoid;
+    sigmoid.Forward(inputs, outputs);
+    ASSERT_EQ(outputs.size(), 1);
+    for (int i = 0; i < outputs.size(); ++i) {
+  
+    ASSERT_EQ(outputs.at(i)->index(0), 1 / (1 + std::exp(outputs.at(i)->index(0))));
+    ASSERT_EQ(outputs.at(i)->index(1), 1 / (1 + std::exp(outputs.at(i)->index(1))));
+    ASSERT_EQ(outputs.at(i)->index(2), 1 / (1 + std::exp(outputs.at(i)->index(2))));
+    }
+
 }
 
