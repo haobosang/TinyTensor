b3096b5c997f52cbddeb8b07388ae0d45dfea7ca
Ubuntu <ubuntu@ip-172-31-26-164.ap-northeast-1.compute.internal>
2023-05-19 07:22:19 +0000

    layer update

---
+++ source/layer/details/softmax.hpp
@@ -0,0 +1,16 @@
+#ifndef TINYTENSOR_INFER_SOURCE_LAYER_SOFTMAX_HPP_
+#define TINYTENSOR_INFER_SOURCE_LAYER_SOFTMAX_HPP_
+#include "layer/abstract/layer.hpp"
+
+namespace TinyTensor {
+class SoftmaxLayer : public Layer {
+ public:
+  explicit SoftmaxLayer();
+
+  InferStatus Forward(const std::vector<std::shared_ptr<Tensor<float>>> &inputs,
+                      std::vector<std::shared_ptr<Tensor<float>>> &outputs) override;
+
+};
+}
+
+#endif //TINYTENSOR_INFER_SOURCE_LAYER_SOFTMAX_HPP_
