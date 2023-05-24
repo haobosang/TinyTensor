f6e352f0bc2104c6ea50e1d7e0603d298f32ca73
Ubuntu <ubuntu@ip-172-31-26-164.ap-northeast-1.compute.internal>
2023-05-24 02:13:03 +0000

    read cv update

--- include/data/load_data.hpp
+++ include/data/load_data.hpp
@@ -9,12 +9,10 @@ namespace TinyTensor
     
 class CSVDataLoader{
 public:
-    static std::shared_ptr<Tensor<float>> LoadData(const std::string &file_path , char split_char = ',');
-    static std::shared_ptr<Tensor<float>> LoadDataWithHeader(const std::string &file_path, 
-    std::vector<std::string> &header,char split_char = ',');
+    static arma::fmat LoadData(const std::string &file_path, char split_char = ',');
     
 private:
-    static std::pair<size_t,size_t> GetMatrixSize(std::ifstream &file,char split_char);
+     static std::pair<size_t, size_t> GetMatrixSize(std::ifstream &file, char split_char);
 };
 
 
--- source/data/load_data.cpp
+++ source/data/load_data.cpp
@@ -5,136 +5,89 @@
 #include <glog/logging.h>
 
 namespace TinyTensor {
-std::shared_ptr<Tensor<float >> CSVDataLoader::LoadDataWithHeader(const std::string &file_path,
-                                                                  std::vector<std::string> &headers,
-                                                                  char split_char) {
-    CHECK(!file_path.empty()) << "File path is empty!";
-    std::ifstream in(file_path);
-    CHECK(in.is_open() && in.good()) << "File open failed! " << file_path;
+arma::fmat CSVDataLoader::LoadData(const std::string &file_path, const char split_char) {
+  arma::fmat data;
+  if (file_path.empty()) {
+    LOG(ERROR) << "CSV file path is empty: " << file_path;
+    return data;
+  }
 
-    std::string line_str;
-    std::stringstream line_stream;
+  std::ifstream in(file_path);
+  if (!in.is_open() || !in.good()) {
+    LOG(ERROR) << "File open failed: " << file_path;
+    return data;
+  }
 
-    const auto &[rows, cols] = CSVDataLoader::GetMatrixSize(in, split_char);
-    CHECK(rows >= 1);
-    std::shared_ptr<Tensor<float>> input_tensor = std::make_shared<Tensor<float>>(1, rows - 1, cols);
-    arma::fmat &data = input_tensor->at(0);
+  std::string line_str;
+  std::stringstream line_stream;
 
-    size_t row = 0;
-    while (in.good()) {
-        std::getline(in, line_str);
-        if (line_str.empty()) {
-        break;
-        }
+  const auto &[rows, cols] = CSVDataLoader::GetMatrixSize(in, split_char);
+  data.zeros(rows, cols);
 
-        std::string token;
-        line_stream.clear();
-        line_stream.str(line_str);
-
-        size_t col = 0;
-        while (line_stream.good()) {
-        std::getline(line_stream, token, split_char);
-        try {
-            //todo 
-            // 能够读取到第一行的csv列名，并存放在headers中
-            if(row==0) headers.push_back(token);
-            else{
-                data.at(row-1, col) = std::stof(token);
-            }
-
-            // 能够读取到第二行之后的csv数据，并相应放置在data变量的row，col位置中
-        }
-        catch (std::exception &e) {
-            LOG(ERROR) << "Parse CSV File meet error: " << e.what();
-            continue;
-        }
-        col += 1;
-        CHECK(col <= cols) << "There are excessive elements on the column";
-        }
-
-        row += 1;
-        CHECK(row <= rows) << "There are excessive elements on the row";
+  size_t row = 0;
+  while (in.good()) {
+    std::getline(in, line_str);
+    if (line_str.empty()) {
+      break;
     }
-    return input_tensor;
-}
 
-std::shared_ptr<Tensor<float >> CSVDataLoader::LoadData(const std::string &file_path, char split_char) {
-    CHECK(!file_path.empty()) << "File path is empty!";
-    std::ifstream in(file_path);
-    CHECK(in.is_open() && in.good()) << "File open failed! " << file_path;
+    std::string token;
+    line_stream.clear();
+    line_stream.str(line_str);
 
-    std::string line_str;
-    std::stringstream line_stream;
-
-    const auto &[rows, cols] = CSVDataLoader::GetMatrixSize(in, split_char);
-    std::shared_ptr<Tensor<float>> input_tensor = std::make_shared<Tensor<float>>(1, rows, cols);
-    arma::fmat &data = input_tensor->at(0);
-
-    size_t row = 0;
-    while (in.good()) {
-        std::getline(in, line_str);
-        if (line_str.empty()) {
-        break;
-        }
-
-        std::string token;
-        line_stream.clear();
-        line_stream.str(line_str);
-
-        size_t col = 0;
-        while (line_stream.good()) {
-        std::getline(line_stream, token, split_char);
-        try {
-            data.at(row, col) = std::stof(token);
-        }
-        catch (std::exception &e) {
-            LOG(ERROR) << "Parse CSV File meet error: " << e.what();
-            continue;
-        }
-        col += 1;
-        CHECK(col <= cols) << "There are excessive elements on the column";
-        }
-
-        row += 1;
-        CHECK(row <= rows) << "There are excessive elements on the row";
+    size_t col = 0;
+    while (line_stream.good()) {
+      std::getline(line_stream, token, split_char);
+      try {
+        data.at(row, col) = std::stof(token);
+      }
+      catch (std::exception &e) {
+        DLOG(ERROR) << "Parse CSV File meet error: " << e.what() << " row:" << row << " col:" << col;
+      }
+      col += 1;
+      CHECK(col <= cols) << "There are excessive elements on the column";
     }
-    return input_tensor;
+
+    row += 1;
+    CHECK(row <= rows) << "There are excessive elements on the row";
+  }
+  return data;
 }
 
 std::pair<size_t, size_t> CSVDataLoader::GetMatrixSize(std::ifstream &file, char split_char) {
-    bool load_ok = file.good();
-    file.clear();
-    size_t fn_rows = 0;
-    size_t fn_cols = 0;
-    const std::ifstream::pos_type start_pos = file.tellg();
+  bool load_ok = file.good();
+  file.clear();
+  size_t fn_rows = 0;
+  size_t fn_cols = 0;
+  const std::ifstream::pos_type start_pos = file.tellg();
 
-    std::string token;
-    std::string line_str;
-    std::stringstream line_stream;
+  std::string token;
+  std::string line_str;
+  std::stringstream line_stream;
 
-    while (file.good() && load_ok) {
-        std::getline(file, line_str);
-        if (line_str.empty()) {
-        break;
-        }
-
-        line_stream.clear();
-        line_stream.str(line_str);
-        size_t line_cols = 0;
-
-        std::string row_token;
-        while (line_stream.good()) {
-        std::getline(line_stream, row_token, split_char);
-        ++line_cols;
-        }
-        if (line_cols > fn_cols) {
-        fn_cols = line_cols;
-        }
-
-        ++fn_rows;
+  while (file.good() && load_ok) {
+    std::getline(file, line_str);
+    if (line_str.empty()) {
+      break;
     }
-    file.clear();
-    file.seekg(start_pos);
-    return {fn_rows, fn_cols};
+
+    line_stream.clear();
+    line_stream.str(line_str);
+    size_t line_cols = 0;
+
+    std::string row_token;
+    while (line_stream.good()) {
+      std::getline(line_stream, row_token, split_char);
+      ++line_cols;
     }
+    if (line_cols > fn_cols) {
+      fn_cols = line_cols;
+    }
+
+    ++fn_rows;
+  }
+  file.clear();
+  file.seekg(start_pos);
+  return {fn_rows, fn_cols};
+}
 }
