aebb2ef3aa860432bfd2d1c9835861bd82aac1c0
PMSang <pm9528@foxmail.com>
2023-05-30 01:11:19 +0000

    fix files

--- build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pm/Desktop/TinyTensor/test
-# Build directory: /home/pm/Desktop/TinyTensor/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
